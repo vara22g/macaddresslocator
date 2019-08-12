391078242ff4420d90ace9e7616089bb5495dcf2
vara22g <51677818+vara22g@users.noreply.github.com>
2019-08-12 13:23:21 -0400

    Add files via upload

---
+++ macaddresslocator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "START OF SCRIPT"
+
+if [ $# -ne 1 ]; then
+    echo "USAGE: $0 Validaddress"
+    exit 0
+else 
+   {
+    mac=$1 
+    echo "MAC ADDRESS":$mac
+       curl -k http://localhost:8090/mac/search/$mac 
+   }
+fi
+echo "END OF SCRIPT"
